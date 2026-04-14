05005563ff93982773d9f1dd6ba4f57c9be35f3f
billpounds23-spec <bill.pounds23@gmail.com>
2026-04-14 10:38:29 -0500

    Added Supabase connection

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const supabaseUrl = 'https://uuebwzbryczakntelppi.supabase.co'
+const supabaseAnonKey = 'sb_publishable_WV6k8nsHTWBGMhD96AFLmg_PPUGnnZj'
+
+const supabase = window.supabase.createClient(https://uuebwzbryczakntelppi.supabase.co, sb_publishable_WV6k8nsHTWBGMhD96AFLmg_PPUGnnZj)
+
+console.log('Supabase connected:', supabase)
